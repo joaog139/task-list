66148c88147276da0490d314ccdd6ec953ef7ce2
joaog139 <joaogabrielrk09@gmail.com>
2026-08-27 14:37:16 -0300

    redefinindo a senha do abnco de dados

--- BackEndTasks/server.js
+++ BackEndTasks/server.js
@@ -6,7 +6,6 @@ const { Pool } = pg;
 
 const pool = new Pool({
   connectionString: process.env.REACT_APP_CONECTION_STRING,
-  //"postgresql://neondb_owner:npg_z07uYOSCBaDV@ep-divine-queen-ay7tl3za-pooler.c-5.us-east-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
   ssl: {
     rejectUnauthorized: false,
   },
